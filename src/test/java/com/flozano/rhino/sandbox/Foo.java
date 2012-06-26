1d08e458640319166d2bf621dd86e356239d01a6
Francisco A. Lozano <francisco.lozano@kii.com>
2012-06-26 04:02:59 +0200

    Add internal usage of externally forbidden class

--- src/test/java/com/flozano/rhino/sandbox/Foo.java
+++ src/test/java/com/flozano/rhino/sandbox/Foo.java
@@ -1,11 +1,16 @@
 package com.flozano.rhino.sandbox;
 
+import java.util.HashMap;
+
 public class Foo {
 
 	public static final int VALUE = 1234;
 
 	public static void doSomething() {
-		System.out.println("Hola hola caracola");
+		// Do some operations with a class which cannot be used directly
+		HashMap<Object, Object> map = new HashMap<Object, Object>();
+		map.put("1234", "1234");
+		System.out.println("Hello world " + map.get("1234"));
 	}
 
 	public static void printSomething(int val) {
