4e9f97e5ba53dc59edd7223c63592bb86b4edc28
Francisco A. Lozano <francisco.lozano@kii.com>
2012-06-26 03:54:42 +0200

    Added some docs to SandboxShutter interface

--- src/main/java/com/flozano/rhino/sandbox/SandboxShutter.java
+++ src/main/java/com/flozano/rhino/sandbox/SandboxShutter.java
@@ -1,15 +1,52 @@
 package com.flozano.rhino.sandbox;
 
-public interface SandboxShutter
-{
-   public boolean allowClassAccess(Class<?> type);
+public interface SandboxShutter {
+	/**
+	 * Control access to a given class
+	 * 
+	 * @param type
+	 * @return
+	 */
+	public boolean allowClassAccess(Class<?> type);
 
-   public boolean allowFieldAccess(Class<?> type, Object instance, String fieldName);
+	/**
+	 * Control access to a given field in an instance
+	 * 
+	 * @param type
+	 * @param instance
+	 * @param fieldName
+	 * @return
+	 */
+	public boolean allowFieldAccess(Class<?> type, Object instance,
+			String fieldName);
 
-   public boolean allowMethodAccess(Class<?> type, Object instance, String methodName);
+	/**
+	 * Control access to a given method in an instance
+	 * 
+	 * @param type
+	 * @param instance
+	 * @param methodName
+	 * @return
+	 */
+	public boolean allowMethodAccess(Class<?> type, Object instance,
+			String methodName);
 
-   public boolean allowStaticFieldAccess(Class<?> type, String fieldName);
+	/**
+	 * Control access to an static field of a given class
+	 * 
+	 * @param type
+	 * @param fieldName
+	 * @return
+	 */
+	public boolean allowStaticFieldAccess(Class<?> type, String fieldName);
 
-   public boolean allowStaticMethodAccess(Class<?> type, String methodName);
-   
+	/**
+	 * Control access to an static method of a given class
+	 * 
+	 * @param type
+	 * @param methodName
+	 * @return
+	 */
+	public boolean allowStaticMethodAccess(Class<?> type, String methodName);
+
 }
